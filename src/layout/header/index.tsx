6ef094250296c0488f930d5f593675f7524f21f1
Munei Nengwenani <munei.nengwenani@invictuscapital.com>
2020-08-12 05:38:49 +0200

    Removing unwanted files , adding basic structure and sample layouts with components for user details

--- src/layout/header/index.tsx
+++ src/layout/header/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { PageHeader } from 'antd';
 
-import './styles.less';
+import './styles.scss';
 
 const Header = () => {
   const { t } = useTranslation('header');
