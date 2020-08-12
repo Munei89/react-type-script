e9b85b33c08c25af0f37623c71ab7aa9848887b1
Munei Nengwenani <munei.nengwenani@invictuscapital.com>
2020-08-12 09:58:36 +0200

    fixing display and fixing yarn issues

--- src/screens/reports/reports.tsx
+++ src/screens/reports/reports.tsx
@@ -1,15 +1,8 @@
 import React from 'react';
-import { Table, Tag } from 'antd';
-import { columns, dataSource } from '@constants/reports';
-
 type Props = {};
 
 const Reports: React.FC<Props> = () => {
-  return (
-    <>
-      <div className="reports">Reports</div>
-    </>
-  );
+  return <div className="reports">Reports</div>;
 };
 
 export default Reports;
